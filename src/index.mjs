2fa00e7d1d49e871d67158fe2996e23201651105
Vinu Devaraj <vinudevaraj007@gmail.com>
2021-01-12 20:15:22 +0530

    npx tested

---
+++ src/index.mjs
@@ -0,0 +1,2 @@
+let c = 12;
+console.log('hello mate');
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ var path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './foo.js',
+  entry: './src/index.mjs',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'foo.bundle.js'
+    filename: 'main.js'
   }
 };
